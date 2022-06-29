9b81ae2ec2beb9a91b18f37ab8faa5b9eb552235
Csylvainc <81378896+Csylvainc@users.noreply.github.com>
2022-06-29 16:35:42 +0200

    add inscription et connexion

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Nav from './components/Nav'
+import Home from './components/Home'
+import Inscription from './components/Inscription'
+import Connexion from './components/Connexion'
+import Salle from './components/Salle'
 
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <>
+      <Nav />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="inscription" element={<Inscription />} />
+          <Route path="connexion" element={<Connexion />} />
+          <Route path="salle" element={<Salle />} />
+        </Routes>
+      </div>
+    </>
+
   );
 }
-
-export default App;
