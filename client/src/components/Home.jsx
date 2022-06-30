a5620d2b6515e05ded713e0ff6aa2f6646bb2703
slami <lamislimane@gmail.com>
2022-06-30 15:26:25 +0200

    Get all rooms

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Salle from "./Salle";
 
 function Home() {
+  const [salles, setSalles] = useState([]);
+
+  const getSalles = () => {
+    fetch("http://localhost:5000/salles")
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        setSalles(result);
+      })
+      .catch((error) => console.log(error));
+  };
+
+
+  useEffect(() => {
+    getSalles();
+  }, []);
+
   return (
     <>
       <h1 className="title">Bienvenue sur le site de résa Escape Room</h1>
-      <Salle />
+      {salles.map((salle) => {
+        return <Salle salle={salle} key={salle._id}/>;
+      })}
     </>
   );
 }
--- server.js
+++ server.js
@@ -1,29 +1,26 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose';
-import dotenv from 'dotenv'
-import routerSalle from './routes/salle.js'
-dotenv.config({ path: "./config.env" })
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import routerSalle from "./routes/salle.js";
+dotenv.config({ path: "./config.env" });
 
 const app = express();
 const port = process.env.PORT;
-const URI = process.env.ATLAS_URI
+const URI = process.env.ATLAS_URI;
 
-app.use('/salles', routerSalle)
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
+app.use("/salles", routerSalle);
 
-main().catch(err => console.error(err))
+main().catch((err) => console.error(err));
 async function main() {
-    await mongoose.connect('mongodb+srv://loukansi:Ssalam46@cluster0.2pf9yjw.mongodb.net/escapeGameDB?retryWrites=true&w=majority');
-    console.log("connection réussi");
-
+  mongoose.connect(URI);
+  console.log("connection réussi");
 }
 
 app.listen(port, () => {
-
-    console.log(`Server is running on port: ${port}`);
-
+  console.log(`Server is running on port: ${port}`);
 });
